f711877c6c7bceec17af939417100f0aa4d2a34d
Sam Orme <ormesam@hotmail.com>
2020-09-01 12:32:12 +0100

    Add example for server side and wasm

--- src/WasmExample/Program.cs
+++ src/WasmExample/Program.cs
@@ -1,14 +1,10 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
-namespace Example {
+namespace WasmExample {
     public class Program {
         public static async Task Main(string[] args) {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
