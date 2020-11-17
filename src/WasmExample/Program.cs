11025ae103fad55a77c4e200193bf214ebb669d3
Sam Orme <ormesam@hotmail.com>
2020-11-17 21:16:19 +0000

    Update to .NET 5.0

--- src/WasmExample/Program.cs
+++ src/WasmExample/Program.cs
@@ -10,7 +10,7 @@ namespace WasmExample {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
